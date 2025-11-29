acb50ea852bbab2704a004397404558d2d274d1f
hesamb1981 <hesam.b1981@gmail.com>
2025-11-29 11:23:49 +0000

    Update nsi-demo.js

--- netlify/functions/nsi-demo.js
+++ netlify/functions/nsi-demo.js
@@ -1,1 +1,24 @@
+exports.handler = async () => {
+  try {
+    const demoResponse = {
+      engine: "NSI",
+      status: "ok",
+      narrative: "This is a demo NSI Engine response. Connectivity working."
+    };
 
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(demoResponse, null, 2)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "NSI demo failed", details: err.message })
+    };
+  }
+};
